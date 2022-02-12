b00803b891bbfdf3ee230559629bd9098eca2f50
Ezabulbary <ezabul.bary@gmail.com>
2022-02-12 21:58:04 +0600

    math

--- math.js
+++ math.js
@@ -1,6 +1,29 @@
-const myNumber = [12, 22, 23, 25, 41, 52, 63, 54, 56, 35, 59];
+// Math.abs 
 
-let numbers = 1;
-function sumNumber(){
-    if()
-}
+const myNumber = -21;
+const output = Math.abs(myNumber);
+console.log(output);
+
+// Math.ceil 
+
+const mySecNumber = 8.1078;
+const SecOutput = Math.ceil(mySecNumber);
+console.log(SecOutput);
+
+// Math.floor 
+
+const myThirdNumber = 8.912;
+const thirdOutput = Math.floor(myThirdNumber);
+console.log(thirdOutput);
+
+// Math.round
+
+const myRoundNumber = 3.5932;
+const roundOutput = Math.round(myRoundNumber);
+console.log(roundOutput);
+
+// Math.random
+
+const randomOutput = Math.random()*7;
+const myRoundOutput = Math.round(randomOutput);
+console.log(myRoundOutput);
