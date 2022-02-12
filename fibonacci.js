5b5c230391f520f44d8816ea9abcd890acf130d9
Ezabulbary <ezabul.bary@gmail.com>
2022-02-13 01:45:29 +0600

    fibonacci series added with finction

--- fibonacci.js
+++ fibonacci.js
@@ -1,4 +1,4 @@
-/* const fiboSirrige = [0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144]; */
+/* const fiboSeries = [0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144]; */
 
 const fibo = [0, 1];
 for(let i = 2; i <= 20; i++){
@@ -6,3 +6,22 @@ for(let i = 2; i <= 20; i++){
 }
 
 console.log(fibo);
+
+// fibonacci Series with function 
+
+function fibonacciSeries(num) {
+    if(typeof num != 'number'){
+        return 'Plese input a valid number';
+    }
+    else if(num < 2){
+        return 'plese input a positive number geter than one';
+    }
+    const fibo = [0, 1];
+    for (let i = 2; i <= num; i++) {
+        fibo[i] = fibo[i - 1] + fibo[i - 2];
+    }
+    return fibo;
+}
+
+const fiboSeries = fibonacciSeries(30);
+console.log(fiboSeries);
